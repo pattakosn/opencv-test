79f7a685ee6c22d93455de47da9d9d6d5f28db2f
N. Pattakos <pattakosn@fastmail.com>
2023-12-27 16:31:33 +0100

    compile with opencv and load a sample png

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 using namespace cv;
+#include "top_level_path.h"
+
 int main()
 {
-    std::string image_path = "path/to/image";
+    std::string image_path = top_level_path_str + "/lena.png";
     Mat img = imread(image_path, IMREAD_COLOR);
 
     imshow("Display window", img);
